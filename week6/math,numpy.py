ee76e45ad48af236db8cd175e4eb3b1b8b3c97c3
olugon <126735508+olugon0821@users.noreply.github.com>
2024-03-11 09:47:26 +0900

    math,numpy module .py

---
+++ week6/math,numpy.py
@@ -0,0 +1,22 @@
+import math
+import numpy as np
+
+num = 3.1415
+
+print(math.floor(num * 10000) / 10000)
+
+print(math.ceil(num * 1000) / 1000) 
+
+print(math.sin(1))
+print(math.cos(1))
+print(math.tan(1))
+
+print()
+
+print(np.floor(num * 10000) / 10000)
+
+print(np.ceil(num * 1000) / 1000) 
+
+print(np.sin(1))
+print(np.cos(1))
+print(np.tan(1))
