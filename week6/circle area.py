be2d1cc36c529c036bccca69110c83f15464ea55
olugon <126735508+olugon0821@users.noreply.github.com>
2024-03-12 11:00:01 +0900

    circle area.py

---
+++ week6/circle area.py
@@ -0,0 +1,18 @@
+class Circle :
+    def __init__ (self, r):
+        r = self.r
+        self.pi = 3.14159
+        
+    def Area (self):
+        return self.pi*(self.r**2)
+    
+    def circum (self):
+        return 2*self.pi*self.r
+
+
+    def toString(self):
+        return "넓이 : {:.4f}\n둘레 : {:.4f}".format(self.getArea(),self.getCircumference())
+
+
+myCircle = Circle(float(input("반지름을 입력하세요 : ")))
+print(myCircle.toString())
